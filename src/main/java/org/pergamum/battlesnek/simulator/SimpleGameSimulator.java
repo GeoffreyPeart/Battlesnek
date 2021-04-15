d900a1578b430a17a5c870f183fe3abd919ad9e6
geoffreypeart <geoffreypeart@localhost>
2021-04-15 09:14:11 -0400

    Refactor the simulator out of the code, and introduce a simulator interface in the plan of letting the strategy for board advancing be extensible.

--- src/main/java/org/pergamum/battlesnek/simulator/SimpleGameSimulator.java
+++ src/main/java/org/pergamum/battlesnek/simulator/SimpleGameSimulator.java
@@ -1,18 +1,27 @@
 package org.pergamum.battlesnek.simulator;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.pergamum.battlesnek.api.Battlesnake;
 import org.pergamum.battlesnek.api.Board;
 import org.pergamum.battlesnek.api.Coordinate;
+import org.pergamum.battlesnek.util.CellContent;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class SimpleGameSimulator implements GameSimulator{
 
 	/**
 	 * 
 	 */
 	public Board advanceBoard(Board board, Coordinate moveInto, boolean growSnake) {
-		Board returnBoard = board.copy(); // TODO deep copy or else
+		log.info(board.toString());
+		log.info(moveInto.toString());
+		log.info(""+ growSnake);
+		
+		Board returnBoard = board.copy(); 
 
 		// moved us
 		Battlesnake you = returnBoard.getSnakes()[0];
@@ -23,29 +32,38 @@ public class SimpleGameSimulator implements GameSimulator{
 		if (growSnake) {
 			offset = 1;
 			Coordinate[] food = returnBoard.getFood();
-			Coordinate[] newFood = new Coordinate[food.length - 1];
+		
+			ArrayList<Coordinate> foodList= new ArrayList<Coordinate>();
+			Arrays.stream(food).forEach(foodItem -> foodList.add(foodItem));
 
-			int f = 0;
-			int nf = 0;
-			while (nf < food.length)
-			{
-				if(food[f].equals(moveInto))
-				{
-					// skip
-				}
-				else
-				{
-					newFood[nf]=food[f];
-					nf++;
-				}
-				f++;
-			}
+			foodList.remove(moveInto);
+			
+			
+			Coordinate[] newFood = new Coordinate[foodList.size()];
+			foodList.toArray(newFood);
+
+			
+//			int f = 0;
+//			int nf = 0;
+//			while (nf < food.length)
+//			{
+//				if(food[f].equals(moveInto))
+//				{
+//					// skip
+//				}
+//				else
+//				{
+//					newFood[nf]=food[f];
+//					nf++;
+//				}
+//				f++;
+//			}
 			returnBoard.setFood(newFood);
 			
-			tempBody = Arrays.copyOfRange(yourBody, 1, yourBody.length);
+			tempBody = Arrays.copyOfRange(yourBody, 0, yourBody.length);
 		} else {
 			offset = 0;
-			tempBody = Arrays.copyOfRange(yourBody, 1, yourBody.length - 1);
+			tempBody = Arrays.copyOfRange(yourBody, 0, yourBody.length-1);
 
 		}
 
@@ -65,6 +83,7 @@ public class SimpleGameSimulator implements GameSimulator{
 		// move the rest of them
 		// TODO
 
+		log.info(returnBoard.toString());
 		return returnBoard;
 	}
 }
--- src/test/java/org/pergamum/battlesnek/simulator/SimpleGameSimulatorTest.java
+++ src/test/java/org/pergamum/battlesnek/simulator/SimpleGameSimulatorTest.java
@@ -1,14 +1,46 @@
 package org.pergamum.battlesnek.simulator;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.pergamum.battlesnek.api.Battlesnake;
+import org.pergamum.battlesnek.api.Board;
+import org.pergamum.battlesnek.api.Coordinate;
+import org.pergamum.battlesnek.util.CellContent;
 
 class SimpleGameSimulatorTest {
 
+	private static final CellContent[][] CELL_CONTENT = {
+			{ CellContent.EMPTY, CellContent.EMPTY, CellContent.EMPTY, CellContent.EMPTY, CellContent.EMPTY },
+			{ CellContent.EMPTY, CellContent.EMPTY, CellContent.EMPTY, CellContent.EMPTY, CellContent.EMPTY },
+			{ CellContent.EMPTY, CellContent.EMPTY, CellContent.EMPTY, CellContent.EMPTY, CellContent.EMPTY },
+			{ CellContent.FOOD, CellContent.EMPTY, CellContent.EMPTY, CellContent.EMPTY, CellContent.EMPTY },
+			{ CellContent.EMPTY, CellContent.SNAKE, CellContent.EMPTY, CellContent.EMPTY, CellContent.EMPTY } };
+
+	private static final Coordinate[] TEST_FOOD = { new Coordinate(0, 0) };
+	private static final Coordinate[] TEST_HAZARDS = { new Coordinate(1, 1) };
+
+	private static final Coordinate[] TEST_SNAKE_BODY = { new Coordinate(3, 3), new Coordinate(3, 4),
+			new Coordinate(2, 4), new Coordinate(1, 4) };
+	private static final Battlesnake TEST_SNAKE = new Battlesnake("TEST ID", "TEST SNAKE", 100, TEST_SNAKE_BODY, "Late",
+			TEST_SNAKE_BODY[0], TEST_SNAKE_BODY[TEST_SNAKE_BODY.length - 1], TEST_SNAKE_BODY.length, "I'M A TEST SNAKE",
+			"SOLO");
+
+	private static final Battlesnake[] TEST_SNAKES = { TEST_SNAKE };
+
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testAdvanceBoard() {
+		// test initialization
+		Board testBoard = new Board(5, 5, TEST_FOOD, TEST_HAZARDS, TEST_SNAKES, null);
+
+		SimpleGameSimulator sgs = new SimpleGameSimulator();
+		Board advancedBoard = sgs.advanceBoard(testBoard,  new Coordinate(3, 2), false);
+		assertNotNull(advancedBoard);
+		assertEquals(CellContent.SNAKE, advancedBoard.whatIsInThatSquare(new Coordinate(3,2)));
+		assertEquals(CellContent.EMPTY, advancedBoard.whatIsInThatSquare(new Coordinate(1,4)));
+		
 	}
 
 }
